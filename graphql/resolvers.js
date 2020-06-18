be0121b51e90ddf76e0aac856e05f111730d9d93
Cyril Cartoux <6ssou@protonmail.com>
2020-06-18 11:21:02 +0200

    create user with graphql

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,30 @@
+const User = require("./../models/user")
+const user = require("./../models/user")
+const bcrypt = require("bcryptjs");
+
 module.exports = {
-    hello() {
-        return {
-            text: 'Hello world',
-            views: 23
+    createUser: async function ({ userInput }, req) {
+        const existingUser = await User.findOne({ email: userInput.email })
+        if (existingUser) {
+            const error = new Error("User exists already")
+            throw error;
+        }
+        try {
+            const hashedPassword = await bcrypt.hash(userInput.password, 12)
+
+            const user = new User({
+                email: userInput.email,
+                password: hashedPassword,
+                name: userInput.name
+            })
+            const createdUser = await user.save()
+            return {...createdUser._doc, _id: createdUser._id.toString()}
+            
+        } catch (err) {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            console.log(err);
         }
     }
 }
